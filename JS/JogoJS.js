f6ac1645aaeae2a52414e3d5aac34153d81d7f73
Braian17-aluno <101749717+Braian17-aluno@users.noreply.github.com>
2022-11-16 09:52:07 -0300

    Update JogoJS.js

--- JS/JogoJS.js
+++ JS/JogoJS.js
@@ -1,5 +1,32 @@
+let a
 let pontos = 10
-let pontuacao = document.querySelector(".pontos")
+let pontuacao = document.querySelector(".pontos");
+let contadorW = 0;
+let contadorP = 0;
+let cont = 0;
+let m
+let z
+let n
+let b 
+document.querySelector("#atirar").addEventListener("click",()=>{botaoatira()});
+document.querySelector("#baixo").addEventListener("click",()=>{move('baixo')});
+document.querySelector("#acima").addEventListener("click", ()=>{move('acima')});
+document.querySelector("#botaoS").addEventListener("click",()=>{iniciar()});
+document.querySelector(".Inst").addEventListener("click",()=>{InstSumir()})
+
+function InstSumir(){
+    let Inst = document.querySelector(".Inst")
+    Inst.setAttribute("class","Sumir")
+}
+
+function iniciar(){
+    m = setInterval("moverdireita1()", 15);
+    z = setInterval("moverdireita2()", 15);
+    n = setInterval("colisao()", 5);
+    cont++
+    let botaoS = document.querySelector("#botaoS");
+    botaoS.parentElement.removeChild(botaoS)
+}
 
 // mover inimigos - automático
 function moverdireita2(){
@@ -14,7 +41,7 @@ function moverdireita2(){
     }
     if ( div2Left >=  fundoWidth - div2Width){
         clearInterval(z);
-        z = setInterval ("moveresquerda2()",15);
+        z = setInterval("moveresquerda2()",15);
     }
 }
 function moveresquerda2(){
@@ -28,13 +55,12 @@ function moveresquerda2(){
         }
     if (div2Left <= 0){
         clearInterval(z);
-        z = setInterval ("moverdireita2()",15);
+        z = setInterval("moverdireita2()",15);
         pontos--
         pontuacao.innerHTML="Pontuação: "+pontos
         if(pontos <= 0){
-            alert("Você perdeu!")
-            alert("Reiniciando...")
-            location.reload();
+            contadorP++
+           reiniciar();
         }
     }
 }
@@ -68,11 +94,31 @@ function moveresquerda1(){
         pontos--
         pontuacao.innerHTML="Pontuação: "+pontos
         if(pontos <= 0){
-            alert("Você perdeu!")
-            alert("Reiniciando...")
-            location.reload();
+            contadorP++
+            reiniciar();
         }
     }
+}
+function reiniciar(){
+    remover();
+    clearInterval(a);
+    clearInterval(n);
+    clearInterval(m);
+    clearInterval(z);
+    let botaoR = document.querySelector(".botaoReplay");
+    botaoR.setAttribute("class","aparece");
+    if(contadorW>0){
+        botaoR.innerHTML="Você venceu! Clique aqui para reiniciar o jogo."
+    }
+    if(contadorP>0){
+        let area = document.querySelector("#area")
+        let player = document.querySelector('.player');
+        area.removeChild(player);
+        botaoR.innerHTML="Você perdeu! Clique aqui para reiniciar o jogo."
+    }
+    botaoR.addEventListener("click",()=>{
+        location.reload();
+    })
 }
 //Player
 let player = document.querySelector('.player');
@@ -85,30 +131,11 @@ player.style.top = 0;
 });
 
 window.addEventListener('keydown', (e) => {
-    //let area = document.querySelector("#area");
-    //let areaWidth = parseInt(getComputedStyle(area).width)
     let fundoHeight = parseInt(getComputedStyle(fundo).height);
-    //let playerLeft= parseInt(getComputedStyle(player).left);
     let playerTop = parseInt(getComputedStyle(player).top);
-  // let playerWidth = parseInt(getComputedStyle(player).width);
     let playerHeight= parseInt(getComputedStyle(player).height);
 
 switch (e.key) {
-  /*
-  case 'a':
-  if (playerLeft <= 0){
-          break;
-  }
-  player.style.left = parseInt(player.style.left) - moveBy + 'vw';
-          break;
-
-  case 'd':
-  if (playerLeft >= areaWidth  - playerWidth){
-          break;
-  }
-  player.style.left = parseInt(player.style.left) + moveBy + 'vw';
-          break;
-  */
   case 'w':
   if (playerTop<= 0){
           break;
@@ -127,50 +154,38 @@ switch (e.key) {
 let atira = new Audio('../Audio/gunshot.mp3')
 let c = 0
 window.addEventListener('keyup', (e) => {
+    if(pontos > 0 && cont >= 1){
     if (e.key == 'q'){
         if (c == 0){
         atirar();
         c++
         }
         if (c == 1){
-            setInterval("atirar()",3000)
+            a = setInterval("atirar()",3000)
             c++
         }
 
     }
+}
 });
 
 function botaoatira(){
+    if(pontos>0 && cont >= 1){
     if (c == 0){
         atirar();
         c++
         }
         if (c == 1){
-            setInterval("atirar()",3000)
+            a = setInterval("atirar()",3000)
             c++
         }
-
+    }
 }
-/*
-if (circle >= fundoWidth  - circleWidth){
-window.addEventListener('load', () => {
-circle.style.position = 'absolute';
-circle.style.left = 10;
-circle.style.top = 0;
-});
-}
-*/
-
-
 // Mover player  - através dos botões
 function move(Direcao) {
     let moveB = 4
-    let area = document.querySelector("#area");
-    let areaWidth = parseInt(getComputedStyle(area).width)
     let fundoHeight = parseInt(getComputedStyle(fundo).height);
-    let playerLeft= parseInt(getComputedStyle(player).left);
     let playerTop = parseInt(getComputedStyle(player).top);
-    let playerWidth = parseInt(getComputedStyle(player).width);
     let playerHeight= parseInt(getComputedStyle(player).height);
 
     switch(Direcao){
@@ -189,28 +204,11 @@ function move(Direcao) {
         }
         player.style.top = parseInt(player.style.top) + moveB + 'vw';
                 break;
-        /*
-        case "esquerda":
-
-        if (playerLeft <= 0){
-                break;
-        }
-        player.style.left = parseInt(player.style.left) - moveB + 'vw';
-                break;
-        
-        case "direita":
-
-        if (playerLeft >= areaWidth  - playerWidth){
-                break;
-        }
-        player.style.left = parseInt(player.style.left) + moveB + 'vw';
-                break;
-                */
     }
 }
+//Disparar tiros
 function atirar(){
     atira.play();
-    //let left = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
     let top = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
     let tiros = document.querySelector(".tiros");
     let tiro = document.createElement("div");
@@ -232,6 +230,7 @@ function atirar(){
 
         if (((tiroLeft >= div1Left)&&(tiroLeft <= div1Left + div1Width))&&
             ((tiroTop >= div1Top)&&(tiroTop <= div1Top + div1Height))){
+
                 para();
                 
         }
@@ -255,46 +254,40 @@ function remover(){
     let tiros = document.querySelector(".tiros");
     tiros.innerHTML=""
 }
-// Transportar Inimigos
+// Transportar Inimigos e Armazenar Info no localStorage
 function para2(){
     pontos+=2
+    b+=1
+    localStorage.setItem('Kills',b)
     let div2 = document.querySelector("#div2")
     div2.style.left = Math.floor(Math.random()*50 + 35)+"vw"
-    div2.style.top = Math.floor(Math.random()*35 + 1)+"vw"
+    div2.style.top = Math.floor(Math.random()*25 + 1)+"vw"
     pontuacao.innerHTML="Pontuação: " + pontos
     if(pontos >= 20){
-        alert("Você venceu!");
-        alert("Reiniciando...");
-        location.reload();
+        contadorW++
+        reiniciar();
     }
    
 }
-function para() {
+function para(){
     pontos+=2
+    b+=1
+    localStorage.setItem('Kills',b)
     let div1 = document.querySelector("#div1")
     div1.style.left = Math.floor(Math.random()*50 + 35)+"vw"
-    div1.style.top = Math.floor(Math.random()*35 + 1)+"vw"
+    div1.style.top = Math.floor(Math.random()*25 + 1)+"vw"
     pontuacao.innerHTML="Pontuação: " + pontos
     if(pontos >= 20){
-        alert("Você venceu!");
-        alert("Reiniciando...");
-        location.reload();
+        contadorW++
+        reiniciar();
     }
    
 }
 
-function para3(){
-    let area = document.querySelector("#area")
-    let player = document.querySelector('.player');
-    area.removeChild(player);
-    alert("Você perdeu!");
-    alert("Reiniciando...");
-    location.reload();
-}
 
-
+//Colisão do player com os inimigos
     function colisao(){
-    
+
     let div1Left= parseInt(getComputedStyle(div1).left);
     let div1Top= parseInt(getComputedStyle(div1).top);
     let div1Height= parseInt(getComputedStyle(div1).height);
@@ -311,42 +304,18 @@ function para3(){
         
         if (((playerLeft >= div1Left)&&(playerLeft <= div1Left + div1Width))&&
             ((playerTop >= div1Top)&&(playerTop <= div1Top + div1Height))){
-                para3();
+                contadorP++
+                reiniciar();
                 
         }
         
         if (((playerLeft >= div2Left)&&(playerLeft <= div2Left + div2Width))&&
             ((playerTop >= div2Top)&&(playerTop <= div2Top + div2Height))){
-                para3();
+                contadorP++
+                reiniciar();
                 
             
         }
         
 
     }
-    
-
-
-
-//Ao carregar a página estas linhas são executadas. 
-
-        let contador = 0;
-        let cont = 0;
-        let m = setInterval("moverdireita1()", 15); //Funções que são chamadas a cada 15 e 5 milisegundos
-        let z = setInterval("moverdireita2()", 15);
-        let n = setInterval("colisao()", 5);
-        
-
-
-document.querySelector("#atirar").addEventListener("click",()=>{botaoatira()});
-//document.querySelector("#esquerda").addEventListener("click",()=>{ move('esquerda')});
-//document.querySelector("#direita").addEventListener("click",()=>{ move('direita')});
-document.querySelector("#baixo").addEventListener("click",()=>{ move('baixo')});
-document.querySelector("#acima").addEventListener("click", ()=>{move('acima')});
-
-alert("Olá! Use as teclas 'w' para se mexer pra cima e 's' para se mexer pra baixo! (ou use as flechas!)");
-alert("Use a tecla 'q'(ou aperte o botão das ervilhas) para COMEÇAR a atirar! Você não vai parar de atirar depois disso.");
-alert("Quando você acertar um inimigo você ganha dois pontos, com 20 pontos você ganha!");
-alert("Se um inimigo tocar no fundo do gramado você perde um ponto, com 0 pontos você perde!");
-alert("Se um inimigo te tocar você perde instantaneamente!");
-alert("Bom Jogo!");
